83017c054a9afa49c904c53ae76d13fa7918e065
sidhu <sidhu@gmail.com>
2025-04-10 10:49:30 +0530

    update bastion.sh

--- vpc-terraform/bastion-server/ec2.tf
+++ vpc-terraform/bastion-server/ec2.tf
@@ -1,23 +1,26 @@
+
 # resource "aws_instance" "bastion" {
-#   ami           = var.ami
-#   instance_type = var.instance_type
-# #    vpc_id  = [var.vpc_id] 
-#   subnet_id = slice(var.public_subnet_ids, 0,1)
-# #    sg_id = [var.sg_id]
-#     security_groups = [slice(var.sg_id, 0,1)]
+#   ami             = var.ami
+#   instance_type   = var.instance_type
+#   subnet_id       = element(var.public_subnet_ids, 0)  # Selects the first subnet
+#   security_groups = [element(var.sg_id, 0)]  # Selects the first security group
+#    key_name = "sidhu@123"
 #   tags = {
 #     Name = "bastion-server"
 #   }
-
-
 # }
+
+
+//  aws eks update-kubeconfig --name <cluster-name> --region <region>
 resource "aws_instance" "bastion" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  subnet_id       = element(var.public_subnet_ids, 0)  # Selects the first subnet
-  security_groups = [element(var.sg_id, 0)]  # Selects the first security group
-   key_name = "sidhu@123"
+  ami             = var.ami                        # Define this in your variables
+  instance_type   = var.instance_type              # Define this in your variables
+  subnet_id       = element(var.public_subnet_ids, 0)  # Selects the first public subnet
+  security_groups = [element(var.sg_id, 0)]           # Selects the first security group
+  key_name        = "sidhu@123"                     # Update with a valid key pair name
+ user_data = base64encode(file("C:/Users/KanaparthiSiddhartha/Documents/DevOps/terraform-sample-project-01/vpc-terraform/bastion-server/bastion.sh"))
+ # user_data                   = base64encode(file("AIML.sh"))
   tags = {
-    Name = "bastion-server"
+    Name = "bastion-server"                        # Helpful for instance identification
   }
 }
